1bdc169a1182aa20fd91acd9b1a7326890c6a8c3
krishnavundakode2212 <109051587+krishnavundakode2212@users.noreply.github.com>
2026-07-04 01:38:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import csv
+
+highest_salary = 0
+employee = ""
+
+with open("employees.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        salary = int(row["Salary"])
+        if salary > highest_salary:
+            highest_salary = salary
+            employee = row["Name"]
+
+print(f"Highest Salary: {employee} - {highest_salary}")
